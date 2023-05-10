ddcbcc98ac2abc428ed0d715bce665f992aa3967
QQ850 <73051094+QQ850@users.noreply.github.com>
2023-05-09 18:29:02 -0700

    Add files via upload

---
+++ sample-tests/lib/ConvertToGregorian_Tests.cs
@@ -0,0 +1,23 @@
+namespace sample_tests;
+using Xunit;
+using sample.lib;
+
+public class ConvertToGregorian_Tests
+{
+    [Fact]
+    //TODO: This test will become obsolete if the featured function is fully implemented.
+    public void NotImplementedYet()
+    {
+        Action act = () => Converter.ConvertToGregorian(int.MinValue);
+        Assert.Throws<NotImplementedException>(act);
+    }
+
+    [Fact]
+    //TODO: Initial "red" test to code logic
+    public void One()
+    {
+        int year = 2023;
+        string words = Converter.ConvertToGregorian(year);
+        Assert.Equal("Saturday, January 28, 2023",words,true);
+    }
+}
